85b918896a091ecf1c8685f06f246261249a307f
leosimao <leonardosc.me@gmail.com>
2022-09-10 00:21:11 -0300

    Agora percebi o erro que cometi em não salvar o ALTER

--- Script_BancoDeDados.sql
+++ Script_BancoDeDados.sql
@@ -3,33 +3,33 @@ CREATE DATABASE projeto_aula;
 USE projeto_aula;
 
 CREATE TABLE estado(
-	id_estado INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,nome VARCHAR(255) NOT NULL UNIQUE
     ,sigla CHAR(2) NOT NULL UNIQUE
     ,ativo CHAR (1) NOT NULL DEFAULT 'S'
 );
 
 CREATE TABLE cidade(
-	id_cidade INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,nome VARCHAR(255) NOT NULL
     ,ativo CHAR(1) NOT NULL DEFAULT 'S'
     ,id_estado INT NOT NULL
-	,CONSTRAINT fk_cidade_estado FOREIGN KEY (id_estado) REFERENCES estado(id_estado)
+	,CONSTRAINT fk_cidade_estado FOREIGN KEY (id_estado) REFERENCES estado(id)
 );
 
 CREATE TABLE fazenda (
-	id_fazenda INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,nome VARCHAR(200) NOT NULL
     ,endereco VARCHAR(255)
     ,cep INT 
 	,ativo CHAR(1) NOT NULL DEFAULT 'S'
     ,area_fazenda DECIMAL(10,2) NOT NULL
     ,id_cidade INT NOT NULL
-    ,CONSTRAINT fk_fazenda_cidade FOREIGN KEY (id_cidade) REFERENCES cidade(id_cidade)
+    ,CONSTRAINT fk_fazenda_cidade FOREIGN KEY (id_cidade) REFERENCES cidade(id)
 );
 
 CREATE TABLE produto(
-	id_produto INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,nome VARCHAR(200) NOT NULL UNIQUE
     ,cod_produto INT NOT NULL UNIQUE
     ,uni_medida CHAR(2) NOT NULL
@@ -37,36 +37,36 @@ CREATE TABLE produto(
 );
 
 CREATE TABLE operacao(
-	id_operacao INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,nome VARCHAR(200) NOT NULL UNIQUE
     ,cod_operacao INT NOT NULL UNIQUE
     ,tipo_operacao VARCHAR(10) NOT NULL DEFAULT "MANUAL"
 );
 
 CREATE TABLE equipe(
-	id_equipe INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,nome VARCHAR(200) NOT NULL UNIQUE
     ,qt_integrantes INT NOT NULL
 
 );
 
 CREATE TABLE estoque(
-	id_estoque INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,cod_estoque INT NOT NULL UNIQUE
     ,desc_estoque VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE equipamento(
-	id_equipamento INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,cod_equipamento INT NOT NULL UNIQUE
     ,tp_equipamento CHAR(1) NOT NULL DEFAULT 'T'
     ,dt_aquisicao DATETIME NOT NULL
     ,id_fazenda INT NOT NULL
-    ,CONSTRAINT fk_equipamento_fazenda FOREIGN KEY (id_fazenda) REFERENCES fazenda(id_fazenda)
+    ,CONSTRAINT fk_equipamento_fazenda FOREIGN KEY (id_fazenda) REFERENCES fazenda(id)
 );
 
 CREATE TABLE aplicacao (
-	id_aplicacao INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,cod_aplicacao INT NOT NULL UNIQUE
     ,dt_aplicacao DATETIME NOT NULL
     ,qt_aplicacao DECIMAL(10, 2) NOT NULL 
@@ -77,22 +77,22 @@ CREATE TABLE aplicacao (
     ,id_equipe INT NOT NULL
     ,id_estoque INT NOT NULL
     ,id_equipamento INT NOT NULL
-    ,CONSTRAINT fk_aplicacao_fazenda FOREIGN KEY (id_fazenda) REFERENCES fazenda(id_fazenda)
-    ,CONSTRAINT fk_aplicacao_produto FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
-    ,CONSTRAINT fk_aplicacao_operacao FOREIGN KEY (id_operacao) REFERENCES operacao(id_operacao)
-    ,CONSTRAINT fk_aplicacao_equipe FOREIGN KEY (id_equipe) REFERENCES equipe(id_equipe)
-    ,CONSTRAINT fk_aplicacao_estoque FOREIGN KEY (id_estoque) REFERENCES estoque(id_estoque)
-    ,CONSTRAINT fk_aplicacao_equipamento FOREIGN KEY (id_equipamento) REFERENCES equipamento(id_equipamento)
+    ,CONSTRAINT fk_aplicacao_fazenda FOREIGN KEY (id_fazenda) REFERENCES fazenda(id)
+    ,CONSTRAINT fk_aplicacao_produto FOREIGN KEY (id_produto) REFERENCES produto(id)
+    ,CONSTRAINT fk_aplicacao_operacao FOREIGN KEY (id_operacao) REFERENCES operacao(id)
+    ,CONSTRAINT fk_aplicacao_equipe FOREIGN KEY (id_equipe) REFERENCES equipe(id)
+    ,CONSTRAINT fk_aplicacao_estoque FOREIGN KEY (id_estoque) REFERENCES estoque(id)
+    ,CONSTRAINT fk_aplicacao_equipamento FOREIGN KEY (id_equipamento) REFERENCES equipamento(id)
 );
 
 CREATE TABLE ccusto(
-	id_ccusto INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
     ,cod_ccusto INT NOT NULL UNIQUE
 	,desc_ccusto VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE custo(
-	id_custo INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT
 	,dt_custo DATETIME NOT NULL
     ,qt_consumo DECIMAL(10, 2) NOT NULL
     ,vl_consumo DECIMAL(10, 2) NOT NULL
@@ -100,11 +100,12 @@ CREATE TABLE custo(
     ,id_aplicacao INT NOT NULL
     ,id_ccusto INT NOT NULL
     ,id_produto INT NOT NULL
-    ,CONSTRAINT fk_custo_aplicacao FOREIGN KEY (id_aplicacao) REFERENCES aplicacao(id_aplicacao)
-    ,CONSTRAINT fk_custo_ccusto FOREIGN KEY (id_ccusto) REFERENCES ccusto(id_ccusto)
-    ,CONSTRAINT fk_custo_produto FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
+    ,CONSTRAINT fk_custo_aplicacao FOREIGN KEY (id_aplicacao) REFERENCES aplicacao(id)
+    ,CONSTRAINT fk_custo_ccusto FOREIGN KEY (id_ccusto) REFERENCES ccusto(id)
+    ,CONSTRAINT fk_custo_produto FOREIGN KEY (id_produto) REFERENCES produto(id)
 );
 
+ALTER TABLE custo CHANGE dt_custo dt_historico DATETIME NOT NULL;
 
 INSERT INTO estado(nome, sigla) VALUES ('PARANA', 'PR');
 INSERT INTO estado(nome, sigla) VALUES ('SAO PAULO', 'SP');
@@ -158,9 +159,9 @@ INSERT INTO ccusto(cod_ccusto, desc_ccusto) VALUES (420001, 'TRATOS CULTURAIS SA
 INSERT INTO ccusto(cod_ccusto, desc_ccusto) VALUES (420002, 'TRATOS CULTURAIS URUPES');
 INSERT INTO ccusto(cod_ccusto, desc_ccusto) VALUES (420003, 'TRATOS CULTURAIS MINUANO');
 
-INSERT INTO custo(dt_custo, qt_consumo, vl_consumo, id_aplicacao, id_ccusto, id_produto) VALUES ('2022-05-30', 320.5, 50.00, 2, 1, 2);
-INSERT INTO custo(dt_custo, qt_consumo, vl_consumo, ativo, id_aplicacao, id_ccusto, id_produto) VALUES ('2022-04-05', 1000, 10.00, 'F', 3, 3, 1);
-INSERT INTO custo(dt_custo, qt_consumo, vl_consumo, id_aplicacao, id_ccusto, id_produto) VALUES ('2022-07-16', 320.5, 50.00, 1, 2, 3);
+INSERT INTO custo(dt_historico, qt_consumo, vl_consumo, id_aplicacao, id_ccusto, id_produto) VALUES ('2022-05-30', 320.5, 50.00, 2, 1, 2);
+INSERT INTO custo(dt_historico, qt_consumo, vl_consumo, ativo, id_aplicacao, id_ccusto, id_produto) VALUES ('2022-04-05', 1000, 10.00, 'F', 3, 3, 1);
+INSERT INTO custo(dt_historico, qt_consumo, vl_consumo, id_aplicacao, id_ccusto, id_produto) VALUES ('2022-07-16', 320.5, 50.00, 1, 2, 3);
 
 SELECT * FROM estado;
 SELECT * FROM cidade;
